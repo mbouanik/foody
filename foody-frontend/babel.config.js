7a08279e9d3df3489aea80660826def86c483a78
mbouanik <mbouanik@student.42.fr>
2024-02-28 14:43:02 -0600

    babel config file removed

--- foody-frontend/babel.config.js
+++ foody-frontend/babel.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-  presets: [
-    // your presets
-    "@babel/preset-env",
-    "@babel/preset-react",
-  ],
-  plugins: [
-    // your plugins
-    "@babel/plugin-proposal-private-methods",
-  ],
-};
+// module.exports = {
+//   presets: [
+//     // your presets
+//     "@babel/preset-env",
+//     "@babel/preset-react",
+//   ],
+//   plugins: [
+//     // your plugins
+//     "@babel/plugin-proposal-private-methods",
+//   ],
+// };
